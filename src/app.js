55fd351e3e1ef2d243ccdaf9e84386fc7ae55fc2
Veronica Lesnar <vll4875@rit.edu>
2017-03-24 12:26:46 -0400

    Add collision and small fixes

--- src/app.js
+++ src/app.js
@@ -3,14 +3,18 @@ const socketio = require('socket.io');
 const xxh = require('xxhashjs');
 const fs = require('fs');
 
+const avatar = fs.readFileSync(`${__dirname}/../client/coin.jpg`);
 const groundImage = fs.readFileSync(`${__dirname}/../client/ground.png`);
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const handler = (request, response) => {
-  if (request.url === '/ground.png') {
+  if (request.url === '/ground.png') {  // Background
     response.writeHead(200, { 'Content-Type': 'image/png' });
     response.end(groundImage);
+  } else if (request.url === '/coin.jpg') { // Avatars
+    response.writeHead(200, { 'Content-Type': 'image/jpg' });
+    response.end(avatar);
   } else {
     fs.readFile(`${__dirname}/../client/index.html`, (err, data) => {
       if (err) {
@@ -37,24 +41,44 @@ io.on('connection', (sock) => {
   playerCount++;
 
   socket.player = {
-    socketid: socket.id,
+    socketid: socket.id,  // Used for easy access to transmit data from server
     hash: xxh.h32(`${socket.id}${Date.now()}`, 0xCAFEBABE).toString(16),
     lastUpdate: new Date().getTime(),
     name: `player${playerCount}`,
-    oldName: `player${playerCount} `,
+    oldName: `player${playerCount} `, // Used for updating username
     choice: 1,
+    x: 0,
+    y: 0,
+    cx: 50, // Used for collision
+    cy: 50, // Used for collision
+    r: 100, // Used for collision
+    prevX: 0,
+    prevY: 0,
+    destX: 0,
+    destY: 0,
+    alpha: 0,
+    moveLeft: false,
+    moveRight: false,
+    moveDown: false,
+    moveUp: false,
   };
 
+  // Adds the socket player data to an array for easier access
   players.push(socket.player);
 
+  // Transmits text from client to all users
   socket.on('textUpdate', (data) => {
     let text = `${socket.player.name}: ${data}`;
 
+    // Checks if a user wants to duel another player
     if (text.startsWith(`${socket.player.name}: /engage`)) {
       text = data;
       socket.emit('updatedText', text);
-      const name = text.split(' ').pop().split(' ').shift();
+      
+      // The name of the opponent the player wants to duel
+      const name = text.split(' ').pop().split(' ').shift();  
 
+      // Lets the opponent know the player wishes to duel
       for (let i = 0; i < players.length; i++) {
         const player = players[i];
         if (player.name === name) {
@@ -66,16 +90,18 @@ io.on('connection', (sock) => {
           return;
         }
       }
-
+      
+      // If the name the player inputted doesn't exist
       text = `${name} does not exist`;
       io.to(socket.player.socketid).emit('updatedText', text);
-    } else {
+    } else {  // Regular text update
       io.sockets.in('room1').emit('updatedText', text);
     }
   });
 
+  // Updates the username of the player
   socket.on('nameUpdate', (data) => {
-    socket.player.oldName = socket.player.name;
+    socket.player.oldName = socket.player.name; // Stores the old username for reference
     socket.player.name = data;
     socket.player.lastUpdate = Date.now();
     const text = `${socket.player.oldName} has changed their name to ${socket.player.name}`;
@@ -83,6 +109,15 @@ io.on('connection', (sock) => {
     io.sockets.in('room1').emit('updatedText', text);
   });
 
+  // Updates any positions
+  socket.on('movementUpdate', (data) => {
+    socket.player = data;
+    socket.player.lastUpdate = Date.now();
+
+    socket.broadcast.to('room1').emit('updatedMovement', socket.player);
+  });
+
+  // Updates the two users involved in a duel
   socket.on('acceptDuel', (data) => {
     let text = `You are dueling ${socket.player.name}`;
     io.to(data.socketid).emit('updatedText', text);
@@ -92,39 +127,42 @@ io.on('connection', (sock) => {
     io.to(socket.player.socketid).emit('chooseHand', data);
   });
 
+  // Lets the original player know the opponent does not wish to duel
   socket.on('rejectDuel', (data) => {
     const text = `${socket.player.name} does not want to duel`;
     io.to(data.socketid).emit('updatedText', text);
   });
 
+  // Determines who wins the game of Rock, Paper, Scissors
+  // TODO: Logic for winning is wrong
+  // TODO: Player and opponent choices come into server incorrectly
   socket.on('chosenHand', (data) => {
     const player = data.player;
-    let opponent = data.opponent;
+    const opponent = data.opponent;
 
     socket.player.choice = player.choice;
     socket.player.lastUpdate = Date.now();
-
-    let text = 'Undetermined error';
     
-    console.log(`${player.name}: ${player.choice}; ${opponent.name}: ${opponent.choice}`);
+    // If the server cannot determine the choices
+    let text = 'Undetermined error';
 
-    if (player.choice === 1 && opponent.choice === 1) {
+    if (player.choice === 1 && opponent.choice === 1) { // Rock vs. Rock
       text = 'There was a tie';
-    } else if (player.choice === 1 && opponent.choice === 2) {
+    } else if (player.choice === 1 && opponent.choice === 2) {  // Rock vs. Paper
       text = `${opponent.name} has won against ${player.name} with paper vs. rock`;
-    } else if (player.choice === 1 && opponent.choice === 3) {
+    } else if (player.choice === 1 && opponent.choice === 3) {  // Rock vs. Scissors
       text = `${player.name} has won against ${opponent.name} with rock vs. scissors`;
-    } else if (player.choice === 2 && opponent.choice === 1) {
+    } else if (player.choice === 2 && opponent.choice === 1) {  // Paper vs. Rock
       text = `${player.name} has won against ${opponent.name} with paper vs. rock`;
-    } else if (player.choice === 2 && opponent.choice === 2) {
+    } else if (player.choice === 2 && opponent.choice === 2) {  // Paper vs. Paper
       text = 'There was a tie';
-    } else if (player.choice === 2 && opponent.choice === 3) {
+    } else if (player.choice === 2 && opponent.choice === 3) {  // Paper vs. Scissors
       text = `${opponent.name} has won against ${player.name} with scissors vs. paper`;
-    } else if (player.choice === 3 && opponent.choice === 1) {
+    } else if (player.choice === 3 && opponent.choice === 1) {  // Scissors vs. Rock
       text = `${opponent.name} has won against ${player.name} with rock vs. scissors`;
-    } else if (player.choice === 3 && opponent.choice === 2) {
+    } else if (player.choice === 3 && opponent.choice === 2) {  // Scissors vs. Paper
       text = `${player.name} has won against ${opponent.name} with scissors vs. paper`;
-    } else if (player.choice === 3 && opponent.choice === 3) {
+    } else if (player.choice === 3 && opponent.choice === 3) {  // Scissors vs. Scissors
       text = 'There was a tie';
     }
 
